056dce712b982f50b3e7e2cc4a97a004f3d630d7
RomaZinkevich <zra1903@mail.ru>
2024-10-29 21:23:37 +0200

    added component with all reviews

---
+++ src/components/ReviewComponent.jsx
@@ -0,0 +1,36 @@
+import PersonIcon from '@mui/icons-material/Person';
+import Rating from '@mui/material/Rating';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+
+const ReviewComponent = ({review}) => {
+    console.log(review)
+    return(
+        <Box sx={{ border: '1px solid #E0E0E0', borderRadius: 2, padding: 2, marginBottom: 2 }}>
+            <Box display="flex" justifyContent="space-between" alignItems="center" className="reviewHeader">
+                <Box display="flex" alignItems="center" className="userInfo">
+                    <PersonIcon
+                        color="primary"
+                        sx={{
+                            backgroundColor: '#C6C7EC',
+                            borderRadius: '50%',
+                            fontSize: '48px',
+                            marginRight: 2,
+                            padding: '8px' // Optional padding for better visibility
+                        }}
+                    />
+                    <Typography variant="h6">{review.userName}</Typography>
+                </Box>
+                <Box className="reviewInfo" textAlign="right">
+                    <Typography variant="subtitle2">{review.date}</Typography>
+                    <Rating name="half-rating" size="large" value={review.rating} precision={0.5} readOnly />
+                </Box>
+            </Box>
+            <Box className="reviewText" marginTop={2}>
+                <Typography variant="body1">{review.comment}</Typography>
+            </Box>
+        </Box>
+    )
+}
+
+export default ReviewComponent;
--- src/components/ReviewsList.jsx
+++ src/components/ReviewsList.jsx
@@ -1,9 +1,12 @@
+import ReviewComponent from "./ReviewComponent.jsx";
 import Box from "@mui/material/Box";
 
 const ReviewsList = ({reviews}) => {
     console.log(reviews);
     return (
-        <h1>Empty!</h1>
+        <Box className="reviews-list">
+            {reviews.map((review) => <ReviewComponent review={review} key={review.id}/>)}
+        </Box>
     )
 }
 
